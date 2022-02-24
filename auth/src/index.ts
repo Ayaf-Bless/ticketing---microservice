2d2bf2b1edf2d27dfdcff41a3b9b51dd5e9ea7a6
Bless Ambele <blessambel1@gmail.com>
2022-02-24 11:10:12 -0930

    adding the ingress handler for the auth-service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express, { json } from "express";
 const app = express();
 app.use(json);
 
+app.get("api/user/me", (req, res) => {
+  res.json("hello");
+});
+
 app.listen(3000, () => {
   console.log("auth service listening on port 30000 !!!");
 });
