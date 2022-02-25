06e05c78c52b8bbdd03c255a2fb7575d723f80c1
Bless Ambele <blessambel1@gmail.com>
2022-02-25 05:27:26 -0930

    adding validation to the signUp handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import { body } from "express-validator";
+import { Response, Request } from "express";
 
 const router = express.Router();
 
-router.post("/api/users/signUp", (req, res) => {
-  res.send("Hello");
-});
+router.post(
+  "/api/users/signUp",
+  [
+    body("email").isEmail().withMessage("email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+  }
+);
 
 export { router as signUp };
