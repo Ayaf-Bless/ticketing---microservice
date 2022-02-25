668aef8cb3cf36723f6ed68a0ee545f247052d0b
Bless Ambele <blessambel1@gmail.com>
2022-02-25 05:12:20 -0930

    adding the skaffold for the auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { json } from "body-parser";
+//Routes
 import { me } from "./routes/me";
+import { signIn } from "./routes/signin";
+import { signUp } from "./routes/signup";
+import { signOut } from "./routes/signout";
 
 const app = express();
 app.use(json());
 
 app.use(me);
+app.use(signIn);
+app.use(signOut);
+app.use(signUp);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
