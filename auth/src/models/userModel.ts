2b0089a8afe264d57687867886750d4f5a833f34
Bless Ambele <blessambel1@gmail.com>
2022-02-27 09:47:38 -0930

    making mongoose and TS to work fine part-3

--- auth/src/models/userModel.ts
+++ auth/src/models/userModel.ts
@@ -4,23 +4,34 @@ interface UserAttrs {
   email: string;
   password: string;
 }
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "email is required"],
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is require"],
+    },
   },
-  password: {
-    type: String,
-    required: [true, "password is require"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-export const User = mongoose.model<any, UserModel>("User", userSchema);
+export const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
