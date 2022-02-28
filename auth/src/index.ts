5d3a2a275ec2ae60d40ed0c8a1bf166839ade3bc
Bless Ambele <blessambel1@gmail.com>
2022-02-27 17:08:18 -0930

    making use of cookie-session to store the JWT

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 //Routes
 import { me } from "./routes/me";
 import { signIn } from "./routes/signin";
@@ -13,7 +14,14 @@ import { errorHandler } from "./middlewares/errorHandler";
 import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(me);
 app.use(signIn);
