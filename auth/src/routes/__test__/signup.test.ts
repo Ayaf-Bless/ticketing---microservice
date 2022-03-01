e53e2ecbf9b8ed7f8f2e569e8583708c12d5d8d0
Bless Ambele <blessambel1@gmail.com>
2022-03-01 05:12:24 -0930

    testing the signup flow part-2

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,13 @@ it("return a 201 on successful signing up", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 status code with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signUp")
+    .send({
+      password: "1234",
+      email: "hello",
+    })
+    .expect(400);
+});
